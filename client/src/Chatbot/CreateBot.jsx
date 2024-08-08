f16952bdca7de94ad6bfc9802c49b144539b7d36
sriram123.boppe@gmail.com <sriram123.boppe@gmail.com>
2024-08-08 17:01:12 +0530

    Added create bot

--- client/src/Chatbot/CreateBot.jsx
+++ client/src/Chatbot/CreateBot.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 import Navbar from "../Dashboard/Navbar";
 
+import "./css/CreateBot.css";
+
 const CreateBot = () => {
+  const handleCreate = () => {};
   return (
     <div>
       <Navbar />
-      <div className="createBot">
-        <form>
+      <div className="create-bot">
+        <form onSubmit={handleCreate}>
           <div>
-            <input type="text" placeholder="Enter your bot name" />
+            <input type="text" placeholder="Enter your bot name" required />
           </div>
           <div>
-            <input type="text" placeholder="Enter your bot personalities" />
+            <input
+              type="text"
+              placeholder="Enter your bot personalities"
+              required
+            />
           </div>
+          <div>
+            <div style={{ margin: "50px" }}>
+              <label htmlFor="bot-image">Upload your bot image</label>
+            </div>
+            <input
+              style={{ display: "none" }}
+              type="file"
+              accept="image/*"
+              alt="Bot Image"
+              id="bot-image"
+            />
+          </div>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
